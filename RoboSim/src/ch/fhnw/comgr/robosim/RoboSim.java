cf1db454eca99e8cac2c4fdab9f5cb9648701ca4
Florian Fankhauser <fankiflo@gmail.com>
2016-01-08 20:25:56 +0100

    Aufgeräumt

--- RoboSim/src/ch/fhnw/comgr/robosim/RoboSim.java
+++ RoboSim/src/ch/fhnw/comgr/robosim/RoboSim.java
@@ -90,55 +90,8 @@ public final class RoboSim {
 			scene.add3DObject(RoboSimMeshUtilities.createWallY(new Vec3(-15, -15, 0), new Vec3(-15, 15, 5), 5));
 			scene.add3DObject(RoboSimMeshUtilities.createWallY(new Vec3(15, -15, 0), new Vec3(15, 15, 5), 5));
 			
-//			try {
-//				final URL obj = RoboSim.class.getResource("s.obj");
-//				//final URL obj = new URL("file:///Users/radar/Desktop/aventador/aventador_red.obj");
-//				//final URL obj = new URL("file:///Users/radar/Desktop/demopolis/berlin_mitte_o2_o3/o2_small.obj");
-//				
-//				
-//				new ObjReader(obj).getMeshes().forEach(mesh -> meshes.add(mesh));
-//				System.out.println("number of meshes before merging: " + meshes.size());
-//				//final List<IMesh> merged = MeshUtilities.mergeMeshes(meshes);
-//				//System.out.println("number of meshes after merging: " + merged.size());
-//				//double[] d={270*(Math.PI/180),0,90*(Math.PI/180),270*(Math.PI/180),90*(Math.PI/180),0};
-//				IMesh tmp1=meshes.get(3);
-//                tmp1.setName("robot1");
-//                IMesh tmp2=meshes.get(4);
-//                tmp2.setName("robot2");
-//                IMesh tmp3=meshes.get(5);
-//                tmp3.setName("robot3");
-//                IMesh tmp4=meshes.get(0);
-//                tmp4.setName("robot4");
-//                IMesh tmp5=meshes.get(1);
-//                tmp5.setName("robot5");
-//                IMesh tmp0=meshes.get(2);
-//                tmp0.setName("robot6");
-//                meshes.set(0,tmp0);
-//				meshes.set(1,tmp1);
-//				meshes.set(2,tmp2);
-//				meshes.set(3,tmp3);
-//				meshes.set(4,tmp4);
-//				meshes.set(5,tmp5);
-//				
-//				
-//				scene.add3DObjects(meshes);
-//
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
 			controller.initRobot();
 			controller.initButtons();
 		});
-//		controller.animate((time, interval) -> {
-//            angle += 2*speed;
-//
-//            List<Mat4> T=R.solve(angle);
-//            for(int i=1;i<T.size();i++){
-//
-//				meshes.get(i).setTransform(T.get(i));
-//
-//			}
-//
-//        });
 	}
 }
--- RoboSim/src/ch/fhnw/comgr/robosim/tools/RobotTool.java
+++ RoboSim/src/ch/fhnw/comgr/robosim/tools/RobotTool.java
@@ -19,62 +19,46 @@ public class RobotTool extends AbstractTool {
 	@Override
 	public void keyPressed(IKeyEvent e) {
 		switch (e.getKeyCode()) {
-	    case IKeyEvent.VK_Q:
-	    	robot.rotate(5, 1);
-	    	break;
-	    case IKeyEvent.VK_A:
-	    	robot.rotate(5, -1);
-	    	break;
-	    case IKeyEvent.VK_W:
-	    	robot.rotate(4, 1);
-	    	break;
-	    case IKeyEvent.VK_S:
-	    	robot.rotate(4, -1);
-	    	break;
-	    case IKeyEvent.VK_E:
-	    	robot.rotate(3, 1);
-	    	break;
-	    case IKeyEvent.VK_D:
-	    	robot.rotate(3, -1);
-	    	break;
-	    case IKeyEvent.VK_R:
-	    	robot.rotate(2, 1);
-	    	break;
-	    case IKeyEvent.VK_F:
-	    	robot.rotate(2, -1);
-	    	break;
-	    case IKeyEvent.VK_T:
-	    	robot.rotate(1, 1);
-	    	break;
-	    case IKeyEvent.VK_G:
-	    	robot.rotate(1, -1);
-	    	break;
-	    case IKeyEvent.VK_Z:
-	    	robot.rotate(0, 1);
-	    	break;
-	    case IKeyEvent.VK_H:
-	    	robot.rotate(0, -1);
-	    	break;
-	    case IKeyEvent.VK_SPACE:
-	    	robot.reset();
-		default:
-			super.keyPressed(e);
-	}
-//		switch (e.getKeyCode()) {
-//		    case IKeyEvent.VK_RIGHT:
-//		        controller.rotateBottom(reso_axis_bottom);
-//		        break;
-//		    case IKeyEvent.VK_LEFT:
-//		        controller.rotateBottom(-reso_axis_bottom);
-//		        break;
-//		    case IKeyEvent.VK_UP:
-//		        controller.rotateMid1(reso_axis_bottom);
-//		        break;
-//		    case IKeyEvent.VK_DOWN:
-//		        controller.rotateMid1(-reso_axis_bottom);
-//		        break;
-//			default:
-//				super.keyPressed(e);
-//		}
-	}
+		    case IKeyEvent.VK_Q:
+		    	robot.rotate(5, 1);
+		    	break;
+		    case IKeyEvent.VK_A:
+		    	robot.rotate(5, -1);
+		    	break;
+		    case IKeyEvent.VK_W:
+		    	robot.rotate(4, 1);
+		    	break;
+		    case IKeyEvent.VK_S:
+		    	robot.rotate(4, -1);
+		    	break;
+		    case IKeyEvent.VK_E:
+		    	robot.rotate(3, 1);
+		    	break;
+		    case IKeyEvent.VK_D:
+		    	robot.rotate(3, -1);
+		    	break;
+		    case IKeyEvent.VK_R:
+		    	robot.rotate(2, 1);
+		    	break;
+		    case IKeyEvent.VK_F:
+		    	robot.rotate(2, -1);
+		    	break;
+		    case IKeyEvent.VK_T:
+		    	robot.rotate(1, 1);
+		    	break;
+		    case IKeyEvent.VK_G:
+		    	robot.rotate(1, -1);
+		    	break;
+		    case IKeyEvent.VK_Z:
+		    	robot.rotate(0, 1);
+		    	break;
+		    case IKeyEvent.VK_H:
+		    	robot.rotate(0, -1);
+		    	break;
+		    case IKeyEvent.VK_SPACE:
+		    	robot.reset();
+			default:
+				super.keyPressed(e);
+		}
+		}
 }
