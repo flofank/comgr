499661dfc3bbc3e40499d33a6db03df2955e6ee8
stolldanielfhnw <daniel.stoll1@students.fhnw.ch>
2016-01-11 00:36:42 +0100

    MyBranch

--- RoboSim/src/ch/fhnw/comgr/robosim/RoboSim.java
+++ RoboSim/src/ch/fhnw/comgr/robosim/RoboSim.java
@@ -33,12 +33,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ch.fhnw.ether.controller.IController;
-
 import ch.fhnw.ether.formats.obj.ObjReader;
 import ch.fhnw.ether.scene.DefaultScene;
 import ch.fhnw.ether.scene.IScene;
 import ch.fhnw.ether.scene.light.DirectionalLight;
 import ch.fhnw.ether.scene.mesh.IMesh;
+import ch.fhnw.ether.ui.Slider;
 import ch.fhnw.ether.view.IView;
 import ch.fhnw.ether.view.gl.DefaultView;
 import ch.fhnw.util.color.RGB;
@@ -48,69 +48,136 @@ import ch.fhnw.util.math.Vec3;
 public final class RoboSim {
 	private static final RGB AMBIENT = RGB.BLACK;
 	private static final RGB COLOR = RGB.WHITE;
-	private float angle = 0;
-	private float speed = 0.3f;
-	
+	private float speed = 0;
+	private float axis[] = { (float) 0.5, (float) 0.5, (float) 0.5, (float) 0.5, (float) 0.5, (float) 0.5, (float) 0.5,
+			(float) 0.5 };
+	private double t1[] = new double[9];
+	private double t2[] = new double[9];
+	static final int FPS_MIN = 0;
+	static final int FPS_MAX = 30;
+	static final int FPS_INIT = 15; // initial frames per second
+	int sim = 0;
+	List<Vec3> Bahn;
+
 	public static void main(String[] args) {
 		new RoboSim();
 	}
 
 	public RoboSim() {
-		// Create controller
 		final List<IMesh> meshes = new ArrayList<>();
-		double[] d={0,0,0,0,0,0,0,0};
-		double[] a={0,0,0,0,0,0,0,0};
-		RobotSolver R=new RobotSolver(d,a);
+		double[] d = { 0, 0, 0.384, -0.384, -0.77, 0, 0, 0, 0, 0, 0 };
+		double[] a = { -0.0, -0.384, 0, 0.39, 0.01, 1.23456, 0, 0, 0, 0, 0, 0, 0 };
+		double[] di = new double[8];// { 0,-0.478, -0.05, 0, -0.425, 0, -0, 0 };
+		double[] ai = new double[8];// { 0,-0.05, -0.425, 0, 0, 0, 0, 0 };
+		double alpha[] = { 0 * (Math.PI / 180), -90 * (Math.PI / 180), 90 * (Math.PI / 180), 0 * (Math.PI / 180),
+				-90 * (Math.PI / 180), 90 * (Math.PI / 180), -90 * (Math.PI / 180), 0 * (Math.PI / 180),
+				0 * (Math.PI / 180) };
+		double alphai[] = new double[alpha.length];
+		for (int i = 0; i < alpha.length - 1; i++) {
+			alphai[i] = 0 + (alpha[i]);
+			ai[i] = 0 + (a[i]);
+			di[i] = 0 + (d[i]);
+
+		}
+		RobotSolver R = new RobotSolver(d, a, alpha);
 		IController controller = new RoboSimController();
 		controller.run(time -> {
 			new DefaultView(controller, 0, 10, 512, 512, IView.INTERACTIVE_VIEW, "Robot Simulation");
-	
+
 			IScene scene = new DefaultScene(controller);
 			controller.setScene(scene);
-			
+
 			scene.add3DObject(new DirectionalLight(new Vec3(0, 0, 1), RGB.BLACK, RGB.RED));
 			scene.add3DObject(new DirectionalLight(new Vec3(0, 1, 0.5), RGB.BLACK, RGB.BLUE));
-	
+
 			try {
-				final URL obj = RoboSim.class.getResource("s.obj");
-				//final URL obj = new URL("file:///Users/radar/Desktop/aventador/aventador_red.obj");
-				//final URL obj = new URL("file:///Users/radar/Desktop/demopolis/berlin_mitte_o2_o3/o2_small.obj");
-				
+				final URL obj = RoboSim.class.getResource("i1.obj");
+
 				new ObjReader(obj).getMeshes().forEach(mesh -> meshes.add(mesh));
 				System.out.println("number of meshes before merging: " + meshes.size());
-				//final List<IMesh> merged = MeshUtilities.mergeMeshes(meshes);
-				//System.out.println("number of meshes after merging: " + merged.size());
-				//double[] d={270*(Math.PI/180),0,90*(Math.PI/180),270*(Math.PI/180),90*(Math.PI/180),0};
-				IMesh tmp1=meshes.get(3);
-				IMesh tmp2=meshes.get(4);
-				IMesh tmp3=meshes.get(5);
-				IMesh tmp4=meshes.get(0);
-				IMesh tmp5=meshes.get(1);
-				IMesh tmp0=meshes.get(2);
-				meshes.set(0,tmp0);
-				meshes.set(1,tmp1);
-				meshes.set(2,tmp2);
-				meshes.set(3,tmp3);
-				meshes.set(4,tmp4);
-				meshes.set(5,tmp5);
-				
-				
+
+				IMesh tmp1 = meshes.get(3);
+				IMesh tmp2 = meshes.get(4);
+				IMesh tmp3 = meshes.get(5);
+				IMesh tmp4 = meshes.get(0);
+				IMesh tmp5 = meshes.get(1);
+				IMesh tmp0 = meshes.get(2);
+				meshes.set(0, tmp0);
+				meshes.set(1, tmp1);
+				meshes.set(2, tmp2);
+				meshes.set(3, tmp3);
+				meshes.set(4, tmp4);
+				meshes.set(5, tmp5);
+
+				for (int i = 1; i < meshes.size(); i++) {
+					// meshes.get(i).setPosition(new Vec3(1,1,1));
+				}
 				scene.add3DObjects(meshes);
-				
+				controller.getUI().addWidget(
+						new Slider(0, 1, "Angle 1", "Axis 2", speed, (slider, view) -> axis[1] = slider.getValue()));
+				controller.getUI().addWidget(
+						new Slider(0, 2, "Angle 2", "Axis 4", speed, (slider, view) -> axis[3] = slider.getValue()));
+				controller.getUI().addWidget(
+						new Slider(0, 3, "Angle 3", "Axis 5", speed, (slider, view) -> axis[4] = slider.getValue()));
+				controller.getUI().addWidget(
+						new Slider(2, 1, "Angle 4", "Axis 6", speed, (slider, view) -> axis[5] = slider.getValue()));
+				controller.getUI().addWidget(
+						new Slider(2, 2, "Angle 5", "Axis 7", speed, (slider, view) -> axis[6] = slider.getValue()));
+				controller.getUI().addWidget(new Slider(2, 3, "Simulation", "Simualtion", speed,
+						(slider, view) -> speed = slider.getValue()));
+				List<Vec3> f1 = R.solveBahn(new Vec3(0, 0, 1.23456), new Vec3(0.8, 0, 0.8));
+				List<Vec3> f2 = R.solveBahn(new Vec3(0.8, 0, 0.8), new Vec3(-0.7, 0.8, 0.8));
+				List<Vec3> f3 = R.solveBahn(new Vec3(-0.7, 0.8, 0.8), new Vec3(-0.7, -0.8, 0.8));
+				List<Vec3> f4 = R.solveBahn(new Vec3(-0.7, -0.8, 0.8), new Vec3(-0.7, -0.8, 0.5));
+				List<Vec3> f5 = R.solveBahn(new Vec3(-0.7, -0.8, 0.5), new Vec3(0, 0, 1.23456));
+				Bahn = new ArrayList<Vec3>();
+				Bahn.addAll(0, f1);
+				Bahn.addAll(Bahn.size(), f2);
+				Bahn.addAll(Bahn.size(), f3);
+				Bahn.addAll(Bahn.size(), f4);
+				Bahn.addAll(Bahn.size(), f5);
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		});
 		controller.animate((time, interval) -> {
-            angle += 2*speed;
-           
-            List<Mat4> T=R.solve(angle);
-            for(int i=1;i<T.size();i++){
-            	
-				meshes.get(i).setTransform(T.get(i));
-				
+			if (speed > 0) {
+				if (sim < Bahn.size()) {
+					if (sim == 0) {
+						Bahn = R.solveBahn(new Vec3(0, 0, 1.23456), new Vec3(0.5, 0.5, 0.5));
+					}
+					List<Mat4> T1 = R.solve(R.solveAngles(Bahn.get(sim)));
+					for (int i = 0; i < meshes.size() - 1; i++) {
+						// meshes.get(i).setTransform(T2.get(i-1).inverse());
+
+						meshes.get(i).setTransform(T1.get(i));
+
+					}
+					sim++;
+					meshes.get(7).setTransform(T1.get(7));
+				} else {
+					sim = 0;
+				}
+
+			} else {
+				for (int i = 0; i < axis.length; i++) {
+					t1[i] = axis[i] * 270 - 135;
+					t2[i] = -(axis[i] * 270 - 135);
+
+				}
+				List<Mat4> T1 = R.solve(t1);
+				for (int i = 0; i < meshes.size() - 1; i++) {
+
+					meshes.get(i).setTransform(T1.get(i));
+
+				}
+				meshes.get(7).setTransform(T1.get(7));
+
 			}
-           
-        });
+
+		});
+
 	}
+
 }
--- RoboSim/src/ch/fhnw/comgr/robosim/RobotSolver.java
+++ RoboSim/src/ch/fhnw/comgr/robosim/RobotSolver.java
@@ -1,4 +1,5 @@
 package ch.fhnw.comgr.robosim;
+
 import ch.fhnw.util.math.Vec3;
 
 import java.util.ArrayList;
@@ -10,34 +11,137 @@ import ch.fhnw.util.math.Mat4;
 
 
 public class RobotSolver {
-double alpha[]={0,0,0,0,0,0,0,0};
+double alpha[];
 double theta[]={0,0,0,0,0,0,0,0};
+double theta_init[]={0 * (Math.PI / 180),0 * (Math.PI / 180), 90 * (Math.PI / 180),
+		90* (Math.PI / 180),0 * (Math.PI / 180),0 * (Math.PI / 180),90 * (Math.PI / 180),0,0};
+double beta[]={0,0 * (Math.PI / 180),0 * (Math.PI / 180),0 * (Math.PI / 180),0 * (Math.PI / 180),0,0,0,0};
 double[] d;
 double[] a;
-public RobotSolver(double[] d,double[] a){
-	
+double l1=0.384;
+double l2=0.386;
+double l3=0.46456;
+double b[]={0,0,0,-0.0,0,0,0,0};
+public RobotSolver(double[] d,double[] a,double[] alpha){
+	this.alpha=alpha;
 	this.d=d;
 	this.a=a;
 }
 
-public List<Mat4> solve(double theta){
-	for(int i=0;i<this.theta.length;i++){
-		this.theta[i]=theta*(Math.PI/180);
+public List<Mat4> solve(double[] theta){
+	for(int i=1;i<this.theta.length;i++){
+		this.theta[i]=(theta[i])*(Math.PI/180)+theta_init[i];
+//		System.out.println("1: "+theta[1]+" 2: "+theta[2]+" 3: "+theta[3]+" 4: "+theta[4]+" 5: "+theta[5]);	
+	}
+	return makeMatrix();
+}
+public double[] solveAngles(Vec3 pos){
+	double[] theta=new double[8];
+	theta[1]=Math.toDegrees(Math.atan(pos.y/pos.x));
+	double hypotenus=Math.sqrt(Math.pow(pos.x, 2)+Math.pow(pos.y,2));
+	double h_temp=0;
+	double z_temp=0;
+	double alpha=0;
+	double beta=0;
+	
+	if((l1+l2)<pos.z){
 		
+		double l_temp=l2+l3;
+		beta=Math.atan(hypotenus/l_temp);
+		while((h_temp-hypotenus-pos.z-z_temp) < 0.00001 && (h_temp-hypotenus-pos.z-z_temp) > -0.00001){
+			
+			double l_t1=l2*Math.sin(beta);
+			double l_t2=pos.z-l_t1;
+			alpha=Math.acos(l_t2/l3);
+			double t_t1=l3*Math.cos(alpha);
+			double t_t2=hypotenus-t_t1;
+			beta=Math.acos(t_t2/l2);
+			
+			double h1=Math.sin(alpha)*l3;
+			double h2=Math.sin(beta)*l2;
+			double z1=Math.cos(alpha)*l3;
+			double z2=Math.cos(beta)*l2;
+			if(hypotenus>l3||pos.z>l1){
+				h_temp=h1+h2;
+				z_temp=z1+z2;
+			}
+			else{
+				h_temp=h1-h2;
+				z_temp=z1-z2;
+			}
+			
+		}
+	}
+	else{
+		double l_temp=pos.z-l1-l2;
+		alpha=Math.atan(hypotenus/l_temp);
 		
+		while((h_temp-hypotenus-pos.z-z_temp) < 0.00001 && (h_temp-hypotenus-pos.z-z_temp) > -0.00001){
+			double t_t1=l3*Math.cos(alpha);
+			double t_t2=hypotenus-t_t1;
+			beta=Math.acos(t_t2/l2);
+			double l_t1=l2*Math.sin(beta);
+			double l_t2=pos.z-l_t1;
+			alpha=Math.acos(l_t2/l3);
+			double h1=Math.sin(alpha)*l3;
+			double h2=Math.sin(beta)*l2;
+			double z1=Math.cos(alpha)*l3;
+			double z2=Math.cos(beta)*l2;
+			if(hypotenus>l3||pos.z>l1){
+				h_temp=h1+h2;
+				z_temp=z1+z2;
+			}
+			else{
+				h_temp=h1-h2;
+				z_temp=z1-z2;
+			}
+		}
 		
 	}
+	theta[3]=Math.toDegrees(beta);
+	theta[4]=Math.toDegrees(alpha);
+	System.out.println("alpha: "+theta[4]+" beta: "+theta[3]+" theta: "+theta[1]);
 	
-	return makeMatrix();
+	return theta;
+}
+public List<Vec3> solveBahn(Vec3 p1,Vec3 p2){
+	List<Vec3> Bahn=new ArrayList<Vec3>();
+	double x=p2.x-p1.x;
+	double y=p2.y-p1.y;
+	double z=p2.z-p1.z;
+	for(int i=0;i<500;i++){
+		Bahn.add(i, new Vec3(p2.x-(x/50*i),p2.y-(y/50*i),p2.z-(z/50*i)));
+	}
+	return Bahn;
 }
 
 public List<Mat4> makeMatrix(){
 	List<Mat4> T=new ArrayList<Mat4>();
+	Mat4 normal=new Mat4();
 	for(int i=0;i<theta.length;i++){
-		T.add(i,new Mat4((float)Math.cos(theta[i]), (float)(-Math.sin(theta[i])*Math.cos(alpha[i])), (float)(Math.sin(theta[i])*Math.sin(alpha[i])), (float)(a[i]*Math.cos(theta[i])),
-						 (float)Math.sin(theta[i]), (float)(Math.cos(theta[i])*Math.cos(alpha[i])),(float)(-Math.cos(theta[i])*Math.sin(alpha[i])),  (float)(a[i]*Math.sin(theta[i])),
-						  0, (float)Math.sin(alpha[i]),	(float)Math.cos(alpha[i]), (float)d[i],
-						  0,	0, 0, 1));
+		
+		
+		Mat4 first=new Mat4((float)Math.cos(theta[i]), (float)-Math.sin(theta[i]), 0, 0,
+							(float)Math.sin(theta[i]), (float)Math.cos(theta[i]), 0, 0,
+							0, 0, 1, 0,
+							0, 0, 0, 1);
+
+		Mat4 second=new Mat4((float)Math.cos(alpha[i]), 0,(float)Math.sin(alpha[i]), 0,
+							0, 1,0, 0,
+							(float)-Math.sin(alpha[i]), 0,(float)Math.cos(alpha[i]), 0,
+				  			0, 0, 0, 1);
+		Mat4 third=new Mat4(1,0,0,0,
+							0, 1,0, 0,
+							0, 0,(float)Math.cos(beta[i]), (float)-Math.sin(beta[i]),
+							0, 0,(float)Math.sin(beta[i]), (float)Math.cos(beta[i])) ;
+		if(i==0){
+			normal=Mat4.multiply(first,second,third,Mat4.translate(0, 0, (float)d[i]),Mat4.translate((float)a[i], 0, 0));
+		}
+		else{
+			normal=Mat4.multiply(normal,first,second,third,Mat4.translate(0, 0, (float)d[i]),Mat4.translate((float)a[i], 0, 0),Mat4.translate(0,(float)b[i], 0));
+		}
+		
+		T.add(i,normal);
 		}
 	return T;
 }
